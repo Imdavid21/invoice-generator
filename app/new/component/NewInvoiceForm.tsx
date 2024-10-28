9e4679b9896168552e5ef7de09e457035a1c3555
Imdavid21 <84083101+Imdavid21@users.noreply.github.com>
2024-10-28 22:48:09 +0530

    Update NewInvoiceForm.tsx

--- app/new/component/NewInvoiceForm.tsx
+++ app/new/component/NewInvoiceForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
 import { UserInputForm } from "@/app/component/form/userInputForm";
 import { FormSteps } from "@/app/component/form/step/fromSteps";
 import { UserDataPreview } from "@/app/new/component/userDataPreview";
@@ -30,16 +31,18 @@ export const NewInvoiceForm = () => {
           <div className="max-w-lg min-h-screen w-full h-full p-4 md:p-12 border-r border-dashed flex flex-col justify-between">
             <div>
               <div className="flex gap-2 items-center">
-                <Image
-                  src="/android-chrome-512x512.png"
-                  width={40}
-                  height={40}
-                  className="rounded-lg"
-                  alt="logo"
-                />
+                <Link href="/">
+                  <Image
+                    src="/android-chrome-512x512.png"
+                    width={40}
+                    height={40}
+                    className="rounded-lg"
+                    alt="logo"
+                  />
+                </Link>
                 <div>
-                  <p className="font-semibold">Invoice Generator</p>
-                  <p className="text-orange-500 text-sm">By Uddalak Das</p>
+                  <p className="font-semibold">Free Invoice Tool</p>
+                  <p className="text-orange-500 text-sm">Made for you, by someone like you.</p>
                 </div>
               </div>
               <UserInputForm />
