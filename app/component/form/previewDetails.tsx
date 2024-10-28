74a6751b506b8d0260bf5c1fcc5b4a27ced647f8
Imdavid21 <84083101+Imdavid21@users.noreply.github.com>
2024-10-28 23:28:31 +0530

    Update previewDetails.tsx

--- app/component/form/previewDetails.tsx
+++ app/component/form/previewDetails.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { CompanyDetailsPreview } from "@/app/component/form/companyDetails/companyDetailsPreview";
 import { InvoiceDetailsPreview } from "@/app/component/form/invoiceDetails/invoiceDetailsPreview";
 import { InvoiceTermsPreview } from "@/app/component/form/invoiceTerms/InvoiceTermsPreview";
@@ -13,81 +12,55 @@ export const PreviewDetails = ({
   paymentDetails,
   invoiceTerms,
   onClick,
-}) => {
-  const [isDesktop, setIsDesktop] = useState(false);
-
-  useEffect(() => {
-    const updateMedia = () => {
-      setIsDesktop(window.innerWidth >= 768);
-    };
-
-    updateMedia(); // Check on initial render
-    window.addEventListener("resize", updateMedia);
-
-    return () => window.removeEventListener("resize", updateMedia);
-  }, []);
-
-  if (!isDesktop) return null; // Only render on desktop
-
-  return (
-    <div className="overflow-x-auto">
-      <div className="w-[595px] h-[842px] bg-white rounded-2xl border border-dashed justify-center items-center">
-        <InvoiceTermsPreview {...invoiceTerms} onClick={onClick} />
-        <div className="border-b  grid grid-cols-2 justify-between border-dashed">
-          <div
-            className="py-4 px-10 border-r border-dashed cursor-pointer relative group"
-            onClick={() => onClick && onClick("1")}
-          >
-            {!!onClick && (
-              <>
-                <ChevronDown className="animate-pulse w-5 h-5 text-orange-500 rotate-[135deg] group-hover:block hidden absolute top-0 left-0" />
-                <ChevronDown className="animate-pulse w-5 h-5 text-orange-500 -rotate-[135deg] group-hover:block hidden absolute top-0 right-0" />
-                <ChevronDown className="animate-pulse w-5 h-5 text-orange-500 rotate-45 group-hover:block hidden absolute bottom-0 left-0" />
-                <ChevronDown className="animate-pulse w-5 h-5 text-orange-500 -rotate-45 group-hover:block hidden absolute bottom-0 right-0 " />
-              </>
-            )}
-            <YourDetailsPreview {...yourDetails} />
-          </div>
-          <div
-            className="py-4 px-10 border-dashed cursor-pointer relative group"
-            onClick={() => onClick && onClick("2")}
-          >
-            {!!onClick && (
-              <>
-                <ChevronDown className="animate-pulse w-5 h-5 text-orange-500 rotate-[135deg] group-hover:block hidden absolute top-0 left-0" />
-                <ChevronDown className="animate-pulse w-5 h-5 text-orange-500 -rotate-[135deg] group-hover:block hidden absolute top-0 right-0" />
-                <ChevronDown className="animate-pulse w-5 h-5 text-orange-500 rotate-45 group-hover:block hidden absolute bottom-0 left-0" />
-                <ChevronDown className="animate-pulse w-5 h-5 text-orange-500 -rotate-45 group-hover:block hidden absolute bottom-0 right-0 " />
-              </>
-            )}
-            <CompanyDetailsPreview {...companyDetails} />
-          </div>
+}: {
+  yourDetails: YourDetails;
+  companyDetails: CompanyDetails;
+  invoiceDetails: InvoiceItemDetails;
+  paymentDetails: PaymentDetails;
+  invoiceTerms: InvoiceTerms;
+  onClick?: (step: string) => void;
+}) => (
+  <div className="overflow-x-auto">
+    <div className="w-[595px] h-[842px] bg-white rounded-2xl border border-dashed justify-center items-center">
+      <InvoiceTermsPreview {...invoiceTerms} onClick={onClick} />
+      <div className="border-b  grid grid-cols-2 justify-between border-dashed">
+        <div
+          className="py-4 px-10 border-r border-dashed cursor-pointer relative group"
+          onClick={() => onClick && onClick("1")}
+        >
+          {!!onClick && (
+            <>
+              <ChevronDown className="animate-pulse w-5 h-5 text-orange-500 rotate-[135deg] group-hover:block hidden absolute top-0 left-0" />
+              <ChevronDown className="animate-pulse w-5 h-5 text-orange-500 -rotate-[135deg] group-hover:block hidden absolute top-0 right-0" />
+              <ChevronDown className="animate-pulse w-5 h-5 text-orange-500 rotate-45 group-hover:block hidden absolute bottom-0 left-0" />
+              <ChevronDown className="animate-pulse w-5 h-5 text-orange-500 -rotate-45 group-hover:block hidden absolute bottom-0 right-0 " />
+            </>
+          )}
+          <YourDetailsPreview {...yourDetails} />
         </div>
-        <div className="flex flex-col justify-between">
-          <div className="border-b justify-between border-dashed">
-            <InvoiceDetailsPreview {...invoiceDetails} onClick={onClick} />
-          </div>
-          <div className="">
-            <PaymentDetailsPreview {...paymentDetails} onClick={onClick} />
-          </div>
+        <div
+          className="py-4 px-10 border-dashed cursor-pointer relative group"
+          onClick={() => onClick && onClick("2")}
+        >
+          {!!onClick && (
+            <>
+              <ChevronDown className="animate-pulse w-5 h-5 text-orange-500 rotate-[135deg] group-hover:block hidden absolute top-0 left-0" />
+              <ChevronDown className="animate-pulse w-5 h-5 text-orange-500 -rotate-[135deg] group-hover:block hidden absolute top-0 right-0" />
+              <ChevronDown className="animate-pulse w-5 h-5 text-orange-500 rotate-45 group-hover:block hidden absolute bottom-0 left-0" />
+              <ChevronDown className="animate-pulse w-5 h-5 text-orange-500 -rotate-45 group-hover:block hidden absolute bottom-0 right-0 " />
+            </>
+          )}
+          <CompanyDetailsPreview {...companyDetails} />
+        </div>
+      </div>
+      <div className="flex flex-col justify-between">
+        <div className="border-b justify-between border-dashed">
+          <InvoiceDetailsPreview {...invoiceDetails} onClick={onClick} />
+        </div>
+        <div className="">
+          <PaymentDetailsPreview {...paymentDetails} onClick={onClick} />
         </div>
       </div>
     </div>
-  );
-};
-
-// DemoPreview Component
-const DemoPreview = () => (
-  <div className="mx-auto w-full flex justify-center items-center">
-    <PreviewDetails
-      yourDetails={defaultValue.yourDetails}
-      companyDetails={defaultValue.companyDetails}
-      invoiceDetails={defaultValue.invoiceDetails}
-      paymentDetails={defaultValue.paymentDetails}
-      invoiceTerms={defaultValue.invoiceTerms}
-      onClick={() => {}} // Added the onClick function as an empty function
-    />
   </div>
 );
-
-export default DemoPreview;
